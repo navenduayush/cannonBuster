685a62700ef904a9317e0a25ac75b048cd9fddb2
Navendu Ayush <navendu@playsimple.in>
2019-12-08 09:21:36 +0530

    removed hellowordscene.h include statement

--- MyCppGame/Classes/AppDelegate.cpp
+++ MyCppGame/Classes/AppDelegate.cpp
@@ -23,7 +23,6 @@
  ****************************************************************************/
 
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
 #include "BackgroundScene.h"
 
 // #define USE_AUDIO_ENGINE 1
