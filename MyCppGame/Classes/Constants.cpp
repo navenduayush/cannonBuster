a181fa7c327e571d0ea826f932cfec8cba5b4e96
Navendu Ayush <navendu@playsimple.in>
2019-12-08 09:19:50 +0530

    Added Constants class to keep constants

---
+++ MyCppGame/Classes/Constants.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Navendu Ayush on 08/12/19.
+//
+
+#include "Constants.h"
+
+const std::string Constants::DUMMY_STRING = "dummy_string";
+const int Constants::DUMMY_INT = 0;
---
+++ MyCppGame/Classes/Constants.h
@@ -0,0 +1,21 @@
+//
+// Created by Navendu Ayush on 08/12/19.
+//
+
+#ifndef TEMPLATECPP_CONSTANTS_H
+#define TEMPLATECPP_CONSTANTS_H
+
+#include <string>
+#include <utility>
+#include "cocos2d.h"
+#include <json/document.h>
+
+
+class Constants {
+public:
+    static const std::string DUMMY_STRING;
+    static const int DUMMY_INT;
+};
+
+
+#endif //TEMPLATECPP_CONSTANTS_H
